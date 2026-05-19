ad393cde5ce18b993df8ac241799993249aede82
Nayan Chandra <nayan.duet.cse@gmail.com>
2026-05-19 17:36:36 +0600

    exceptional handleing

---
+++ Exception.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program3
+{
+    public void Exception() 
+    {
+        
+        try
+        {
+            int[] myNumbers = {1, 2, 3};
+            Console.WriteLine(myNumbers[10]);
+        }
+        catch (Exception e)
+        {
+             Console.WriteLine(e.Message);
+        }
+    }
+}
