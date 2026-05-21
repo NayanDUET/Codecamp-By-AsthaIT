dbd3251ab0af0111e60ef62d340b750100def313
Nayan Chandra <nayan.duet.cse@gmail.com>
2026-05-21 13:46:37 +0600

    Today task added

--- Program.cs
+++ Program.cs
@@ -1,295 +1,108 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace FareEngineAssessment
+class Program
 {
-    
-    public enum TripStatus
+    static void Main()
     {
-        Pending,
-        Paid,
-        Failed
-    }
+        int[] arrayNumbers = { 10, 20, 30 };
 
-   
-    public class InvalidTripException : Exception
-    {
-        public InvalidTripException(string message) : base(message)
+        foreach (int n in arrayNumbers)
         {
-        }
-    }
-
-    public record Passenger(string PassengerId, string Name);
-
-    public interface IPromotion
-    {
-        decimal ApplyDiscount(decimal currentFare);
-    }
-
- 
-    public interface IPaymentService
-    {
-        bool ProcessPayment(string passengerId, decimal amount);
-    }
-
-    public abstract class Vehicle
-    {
-        public string LicensePlate { get; private set; }
-        public decimal BaseFare { get; private set; }
-
-        protected Vehicle(string licensePlate, decimal baseFare)
-        {
-            if (string.IsNullOrWhiteSpace(licensePlate))
-                throw new ArgumentException("License plate cannot be empty.");
-
-            if (baseFare < 0)
-                throw new ArgumentException("Base fare cannot be negative.");
-
-            LicensePlate = licensePlate;
-            BaseFare = baseFare;
+            Console.WriteLine(n);
         }
 
-        public abstract decimal PerKmRate { get; }
-        public abstract decimal PerMinuteRate { get; }
+        List<int> listNumbers = new List<int>();
 
-        public virtual decimal CalculateFare(decimal distanceKms, int durationMinutes)
+        listNumbers.Add(10);
+        listNumbers.Add(20);
+        listNumbers.Add(30);
+
+        Console.WriteLine(listNumbers[1]);
+
+        listNumbers.Remove(20);
+
+        listNumbers.Add(40);
+
+        foreach (int n in listNumbers)
         {
-            decimal fare = BaseFare + (distanceKms * PerKmRate) +(durationMinutes * PerMinuteRate);
-
-            return fare;
-        }
-    }
-
-   
-    public class StandardCar : Vehicle
-    {
-        public StandardCar(string licensePlate, decimal baseFare)
-            : base(licensePlate, baseFare)
-        {
+            Console.WriteLine(n);
         }
 
-        public override decimal PerKmRate => 10m;
-        public override decimal PerMinuteRate => 2m;
-    }
+        Console.WriteLine(listNumbers.Count);
 
+        Console.WriteLine(listNumbers.Contains(30));
 
-    public class LuxurySedan : Vehicle
-    {
-        public decimal LuxuryTax { get; private set; }
+        Dictionary<int, string> students =
+            new Dictionary<int, string>();
 
-        public LuxurySedan(string licensePlate, decimal baseFare, decimal luxuryTax)
-            : base(licensePlate, baseFare)
+        students.Add(1, "Nayan");
+        students.Add(2, "Rahim");
+        students.Add(3, "Karim");
+
+        Console.WriteLine(students[1]);
+
+        foreach (var student in students)
         {
-            LuxuryTax = luxuryTax;
+            Console.WriteLine(student.Key + " " + student.Value);
         }
 
-        public override decimal PerKmRate => 20m;
-        public override decimal PerMinuteRate => 5m;
+        HashSet<int> uniqueNumbers =
+            new HashSet<int>();
 
-        public override decimal CalculateFare(decimal distanceKms, int durationMinutes)
+        uniqueNumbers.Add(10);
+        uniqueNumbers.Add(20);
+        uniqueNumbers.Add(10);
+
+        foreach (int n in uniqueNumbers)
         {
-            decimal fare = base.CalculateFare(distanceKms, durationMinutes);
-
-            fare += LuxuryTax;
-
-            return fare;
-        }
-    }
-
-
-    public class PercentageDiscount : IPromotion
-    {
-        public decimal Percentage { get; private set; }
-
-        public PercentageDiscount(decimal percentage)
-        {
-            if (percentage < 0 || percentage > 100)
-                throw new ArgumentException("Invalid percentage.");
-
-            Percentage = percentage;
+            Console.WriteLine(n);
         }
 
-        public decimal ApplyDiscount(decimal currentFare)
-        {
-            decimal discount = currentFare * (Percentage / 100m);
+        Console.WriteLine(uniqueNumbers.Contains(20));
 
-            return currentFare - discount;
+        Queue<string> customers =
+            new Queue<string>();
+
+        customers.Enqueue("Nayan");
+        customers.Enqueue("Rahim");
+        customers.Enqueue("Karim");
+
+        Console.WriteLine(customers.Peek());
+
+        customers.Dequeue();
+
+        foreach (string customer in customers)
+        {
+            Console.WriteLine(customer);
+        }
+
+        Stack<int> stackNumbers =
+            new Stack<int>();
+
+        stackNumbers.Push(10);
+        stackNumbers.Push(20);
+        stackNumbers.Push(30);
+
+        Console.WriteLine(stackNumbers.Peek());
+
+        stackNumbers.Pop();
+
+        foreach (int n in stackNumbers)
+        {
+            Console.WriteLine(n);
+        }
+
+        List<int> nums = new List<int>()
+        {
+            10, 20, 30, 40, 50
+        };
+
+        var result = nums.Where(x => x > 20);
+
+        foreach (var n in result)
+        {
+            Console.WriteLine(n);
         }
     }
-
-   
-    public class FlatDiscount : IPromotion
-    {
-        public decimal DiscountAmount { get; private set; }
-
-        public FlatDiscount(decimal discountAmount)
-        {
-            if (discountAmount < 0)
-                throw new ArgumentException("Discount cannot be negative.");
-
-            DiscountAmount = discountAmount;
-        }
-
-        public decimal ApplyDiscount(decimal currentFare)
-        {
-            return currentFare - DiscountAmount;
-        }
-    }
-
-    // payment services implements
-
-    public class CreditCardPaymentService : IPaymentService
-    {
-        public bool ProcessPayment(string passengerId, decimal amount)
-        {
-            Console.WriteLine($"Processing payment for Passenger ID: {passengerId}");
-            Console.WriteLine($"Amount: {amount}");
-
-            return true;
-        }
-    }
-
-
-    public class Trip
-    {
-        public Vehicle Vehicle { get; private set; }
-        public Passenger Passenger { get; private set; }
-
-        public decimal DistanceKms { get; private set; }
-        public int DurationMinutes { get; private set; }
-
-        public IPromotion? Promotion { get; private set; }
-
-        public TripStatus Status { get; private set; }
-
-        public Trip(
-            Vehicle vehicle,
-            Passenger passenger,
-            decimal distanceKms,
-            int durationMinutes,
-            IPromotion? promotion = null)
-        {
-            Vehicle = vehicle ?? throw new InvalidTripException("Vehicle cannot be null.");
-
-            Passenger = passenger ?? throw new InvalidTripException("Passenger cannot be null.");
-
-            if (distanceKms < 0)
-                throw new InvalidTripException("Distance cannot be negative.");
-
-            if (durationMinutes <= 0)
-                throw new InvalidTripException("Duration must be greater than zero.");
-
-            DistanceKms = distanceKms;
-            DurationMinutes = durationMinutes;
-            Promotion = promotion;
-
-            Status = TripStatus.Pending;
-        }
-
-       
-        public decimal CalculateFinalFare()
-        {
-            if (Vehicle == null)
-                throw new InvalidOperationException("No vehicle assigned.");
-
-            decimal fare = Vehicle.CalculateFare(DistanceKms, DurationMinutes);
-
-           
-            if (Promotion != null)
-            {
-                fare = Promotion.ApplyDiscount(fare);
-            }
-
-            
-            if (fare < Vehicle.BaseFare)
-            {
-                fare = Vehicle.BaseFare;
-            }
-
-            return fare;
-        }
-
-        
-        public void CompleteTrip(IPaymentService paymentService)
-        {
-            decimal finalFare = CalculateFinalFare();
-
-            bool paymentSuccess =
-                paymentService.ProcessPayment(
-                    Passenger.PassengerId,
-                    finalFare
-                );
-
-            Status = paymentSuccess
-                ? TripStatus.Paid
-                : TripStatus.Failed;
-        }
-    }
-
-
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            
-               
-                Passenger passenger =
-                    new Passenger("IEDL-2117", "Boktiar");
-
-               
-                Vehicle standardCar =
-                    new StandardCar("EIKLD-5484544", 60m);
-
-                Vehicle luxurySedan =
-                    new LuxurySedan("KJDLJI-9884651", 112m, 30m);
-
-              
-                IPromotion percentagePromo =
-                    new PercentageDiscount(10);
-
-                IPromotion flatPromo =
-                    new FlatDiscount(80m);
-
-                IPaymentService paymentService =
-                    new CreditCardPaymentService();
-
-                
-                Trip trip1 = new Trip(
-                    standardCar,
-                    passenger,
-                    40,
-                    50,
-                    percentagePromo
-                );
-
-                decimal fare1 = trip1.CalculateFinalFare();
-
-
-                trip1.CompleteTrip(paymentService);
-
-                Console.WriteLine($"Trip Status: {trip1.Status}");
-                Console.WriteLine();
-
-             
-                Trip trip2 = new Trip(
-                    luxurySedan,
-                    passenger,
-                    15,
-                    30,
-                    flatPromo
-                );
-
-                decimal fare2 = trip2.CalculateFinalFare();
-
-                Console.WriteLine("LUXURY SEDAN.......");
-
-                trip2.CompleteTrip(paymentService);
-
-                Console.WriteLine($"Trip Status: {trip2.Status}");
-                Console.WriteLine();
-
-                
-            }
-            
-        }
 }
