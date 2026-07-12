bd401a5bd321aa3a0189b595288dc08f9cad429c
Nayan Chandra <nayan.duet.cse@gmail.com>
2026-07-12 23:40:02 +0600

    asp.net provide built in IOC container

--- Program.cs
+++ Program.cs
@@ -1,5 +1,19 @@
 ﻿using DI;
+using Microsoft.Extensions.DependencyInjection;
 
 
-var nofication = ObjectFactory<NotificationService>.Get();
+
+
+//register
+var service = new ServiceCollection();
+service.AddTransient<NotificationService>();
+
+service.AddTransient<IEmialService,EmailService>();
+
+var serviceProvider = service.BuildServiceProvider();
+
+
+//var nofication = ObjectFactory<NotificationService>.Get();
+
+var nofication = serviceProvider.GetRequiredService<NotificationService>();
 nofication.Notify();
